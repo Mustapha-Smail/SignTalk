84ce2e27fe645348980fc8becc785d9074182ddf
Mustapha <mustapha.smail@parisnanterre.fr>
2022-09-27 09:31:41 +0200

    chore: fix test example to not connect to JPA DB

--- src/test/java/fr/parisnanterre/miage/SignTalkApplicationTests.java
+++ src/test/java/fr/parisnanterre/miage/SignTalkApplicationTests.java
@@ -1,9 +1,9 @@
 package fr.parisnanterre.miage;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SignTalkApplicationTests {
 
 	@Test
