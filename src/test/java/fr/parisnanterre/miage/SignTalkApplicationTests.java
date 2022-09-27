898f28c9d5360d94ccf1476b7423040f552b63ac
Mustapha <mustapha.smail@parisnanterre.fr>
2022-09-27 09:13:50 +0200

    chore: add code coverage setup

--- src/test/java/fr/parisnanterre/miage/SignTalkApplicationTests.java
+++ src/test/java/fr/parisnanterre/miage/SignTalkApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SignTalkApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void exampleTest() {
+		System.out.println("test should be passed!");
+	}
+
 
 }
