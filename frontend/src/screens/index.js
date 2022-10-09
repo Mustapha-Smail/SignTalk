4004cfabb4db145f4d0612c7ec39568f3d995476
bkrtraore <71319874+bkrtraore@users.noreply.github.com>
2022-10-09 12:35:52 +0200

    QuizzChoice merge (#2)
    * 1st commit of quizz choice
    
    * quizz choice finished

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './Home/Home'
 export { default as Learn } from './Learn/Learn'
 export { default as Alphabet } from './Alphabet/Alphabet'
+export { default as Quizz } from './Quizz/Quizz'
