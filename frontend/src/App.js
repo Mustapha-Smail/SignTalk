fe84c2f3a2ab91979abd2238b588f6f18aa8a6cb
tsgo3 <39001182@parisnanterre.fr>
2022-10-10 14:39:01 +0200

    Ajout page LSF Quizz (refactoring)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Alphabet, Home, Learn, QuizzChoice } from './screens'
+import { Alphabet, Home, Learn, QuizzChoice, QuizzLSF } from './screens'
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
         <Route path='/alphabet' element={<Alphabet/>}></Route>
         <Route path='/learn' element={<Learn />}></Route>  
         <Route path='/quizz' >
+          <Route path='quizzlsf' element={<QuizzLSF />}></Route>  
           <Route path='' element={<QuizzChoice />}></Route>  
         </Route>  
       </Routes>
