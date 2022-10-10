0c304a068a0a7ce564182ca98585fae6a5063e70
Bakari Traoré <bakari.traore@parisnanterre.fr>
2022-10-10 13:59:48 +0200

    Ajout La page du choix du quizz

---
+++ frontend/src/screens/Quizz/QuizzChoice/QuizzChoice.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navbar, QuizzButton } from '../../components'
+
+const Quizz = () => {
+  return (
+    <>
+      <div className="gradient__bg">
+        <Navbar />
+        <div>
+          <QuizzButton text="4 images 1 mot" page="quizzfr" />
+        </div>
+        <div>
+          <QuizzButton text="4 mots 1 image" page="quizzlsf" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Quizz
--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './Home/Home'
 export { default as Learn } from './Learn/Learn'
 export { default as Alphabet } from './Alphabet/Alphabet'
+export { default as QuizzChoice } from './Quizz/QuizzChoice/QuizzChoice'
