37cbb5eb43315970e071c3660e0e67ffdacd85b1
Bakari Traoré <bakari.traore@parisnanterre.fr>
2022-11-30 17:11:40 +0100

    difficulty 4 word 1 image

--- frontend/src/components/Quizz/QuizzLSF/QuizzLSFContainer.jsx
+++ frontend/src/components/Quizz/QuizzLSF/QuizzLSFContainer.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ImageContainer, TextContainer, ButtonContainer } from '../../../components'
 import { notif } from '../../../utils'
+import { Dropdown } from 'react-bootstrap'
 
 import './QuizzLSFContainer.css';
 
@@ -9,42 +10,96 @@ import './QuizzLSFContainer.css';
 
 const QuizzLSFContainer = () => {
 
+  const [difficulte, setDifficulte] = useState("Difficulté")
   const [quizz, setQuizz] = useState({
     multimedia: 'videoId',
-    words: ['word_1', 'word_2', 'word_3', 'word_4'], 
+    words: ['word_1', 'word_2', 'word_3', 'word_4'],
     correctWord: 'word_3'
-    
+
   })
+
+  // const getData = async () => {
+  //   try {
+  //     const { data } = await axios.post(`api/dictionary/quizz/lsf`)
+  //     setQuizz(data)
+
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+
+  // //Permit the synchronization
+  // useEffect(() => {
+  //   getData()
+  // }, [difficulte])
+
+
+  // // Change the game difficulty 
+  // const changeDifficulty = (diff) => {
+  //   setDifficulte(diff);
+  //   getData();
+  // }
 
   const getData = async () => {
     try {
-      const {data}  = await axios.post(`api/dictionary/quizz/lsf`)
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      const category = difficulte !== "Difficulté" ? difficulte : null
+      const { data } = await axios.post(`/api/dictionary/quizz/lsf`, { category }, config)
       setQuizz(data)
-
     } catch (err) {
       console.error(err);
     }
   }
 
+  //Permit the synchronization
   useEffect(() => {
     getData()
-  }, [])
+  }, [difficulte])
+
+
+  // Change the game difficulty 
+  const changeDifficulty = (diff) => {
+    setDifficulte(diff);
+    getData();
+  }
 
   return (
     <>
+        <div className="quizzfr__dropdown">
+          <Dropdown>
+            <Dropdown.Toggle>
+              {difficulte}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => changeDifficulty("facile")}>
+                facile
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => changeDifficulty("moyen")}>
+                moyen
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => changeDifficulty("difficile")}>
+                difficile
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
       <div className="st__quizz">
         <div className="st__quizz-image">
-          <ImageContainer videoId={quizz.multimedia}/>
+          <ImageContainer videoId={quizz.multimedia} />
         </div>
         <div className="st__quizz-content">
           {quizz.words.map((word, index) => (
             <div className={`st__quizz-word${index}`} onClick={() => notif(word, quizz, getData)}>
-              <TextContainer content={word}/>
+              <TextContainer content={word} />
             </div>
           ))}
         </div>
         <div className="st__quizz-button">
-          <ButtonContainer onClickMethod={getData}/>
+          <ButtonContainer onClickMethod={getData} />
         </div>
       </div>
     </>
